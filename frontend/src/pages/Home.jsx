15c0f032a885fafbf748b96e39d7396bc109b777
Vaneez04 <vaneezsaraswat4@gmail.com>
2025-01-28 18:22:03 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import Header from '../components/header'
+import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopTutors from '../components/TopTutors'
-import Banner from '../components/banner'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
